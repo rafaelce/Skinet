fd5070170908da89695fab04e52044df0c525763
Rafael Cruz <rafaeel.ce@gmail.com>
2023-08-07 13:43:18 -0300

    initial commit

---
+++ API/Program.cs
@@ -0,0 +1,33 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+                   new MySqlServerVersion(new Version()));
+            });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
